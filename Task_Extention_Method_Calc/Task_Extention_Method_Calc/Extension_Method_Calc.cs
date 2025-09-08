78f4033c9198e9f142e9cd8cc2adf258fdfd65d4
Omar Elsawy7 <omarmohamedelsawy7@gmail.com>
2025-09-08 09:54:57 +0300

    Add files via upload

---
+++ Task_Extention_Method_Calc/Task_Extention_Method_Calc/Extension_Method_Calc.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_Extention_Method_Calc
+{
+     static class Extension_Method_Calc
+    {
+        public static double add(this double x , double y) => x + y;
+        public static double sub(this double x , double y) => x - y;
+        public static double mul(this double x , double y) => x * y;
+        public static double div(this double x, double y) 
+        {
+            if (y == 0) throw new DivideByZeroException("Can not divider by zero"); 
+            return x / y;
+        }
+    }
+}
---
+++ Task_Extention_Method_Calc/Task_Extention_Method_Calc/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Task_Extention_Method_Calc
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello, World!");
+
+            // using Extension Method and Lambda Expression and BuildIn Delegate
+            double x = 20.5, y = 10.5;
+            //double a = 20.5, b = 10.5;
+
+            Func<double , double , double > Add = (x,y)=> x.add(y);
+            Func<double , double , double > Sub = (x,y)=> x.sub(y);
+            Func<double , double , double > Div = (x,y)=> x.div(y);
+            Func<double , double , double > Mul = (x,y)=> x.mul(y);
+
+            Console.WriteLine(Add(x,y));
+            Console.WriteLine(Sub(x,y));
+            Console.WriteLine(Div(x,y));
+            Console.WriteLine(Mul(x,y));
+
+        }
+    }
+}
